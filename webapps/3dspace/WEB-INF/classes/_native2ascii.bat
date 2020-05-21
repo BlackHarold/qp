e8d2be9cb578eca956dda989d79487836719cc03
20909902 <n.pyschev@ase-ec.ru>
2020-05-21 18:20:58 +0300

    #41003 add Icons for search and properties

---
+++ webapps/3dspace/WEB-INF/classes/_native2ascii.bat
@@ -0,0 +1,8 @@
+set JAVA_HOME="c:\Program Files\Java\jdk1.8.0_161"
+
+%JAVA_HOME%\bin\native2ascii -encoding utf8 LSACommonFrameworkStringResource_ruNative.properties LSACommonFrameworkStringResource_ru.properties
+%JAVA_HOME%\bin\native2ascii -encoding utf8 emxProductLineStringResource_ruNative.properties emxProductLineStringResource_ru.properties
+%JAVA_HOME%\bin\native2ascii -encoding utf8 emxProgramCentralStringResource_ruNative.properties emxProgramCentralStringResource_ru.properties
+
+
+pause
