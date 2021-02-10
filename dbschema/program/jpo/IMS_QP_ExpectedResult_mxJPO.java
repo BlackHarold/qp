e00235a3eae5020dd94c4329a107f0211f36f2ac
Evgeniy Gul <e.gul@ase-ec.ru>
2021-02-10 12:23:35 +0300

    init commit
    fix commit
    
    fix commit

--- dbschema/program/jpo/IMS_QP_ExpectedResult_mxJPO.java
+++ dbschema/program/jpo/IMS_QP_ExpectedResult_mxJPO.java
@@ -7,7 +7,8 @@ import matrix.db.JPO;
 import matrix.util.StringList;
 import org.apache.log4j.Logger;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class IMS_QP_ExpectedResult_mxJPO {
     private static final Logger LOG = Logger.getLogger("IMS_QP_DEP");
@@ -55,20 +56,31 @@ public class IMS_QP_ExpectedResult_mxJPO {
             Map<String, String> relatedExpectedResultIDs = new HashMap<>();
             for (int i = 0; i < relatedResults.size(); i++) {
                 Map expectedResult = (Map) relatedResults.get(i);
-                relatedExpectedResultIDs.put(
-                        (String) expectedResult.get(DomainConstants.SELECT_ID),
-                        (String) expectedResult.get(DomainConstants.SELECT_NAME)
-                );
+                String expectedResultId = (String) expectedResult.get(DomainConstants.SELECT_ID);
+                DomainObject domainObject = new DomainObject(expectedResultId);
+                boolean isAloneOutput = domainObject.getInfoList(ctx, "to[IMS_QP_ExpectedResult2QPTask].from.id").size() == 1;
+
+                if (!isAloneOutput) {
+                    relatedExpectedResultIDs.put(
+                            (String) expectedResult.get(DomainConstants.SELECT_ID),
+                            (String) expectedResult.get(DomainConstants.SELECT_NAME));
+                } else {
+                    mapMessage.put(domainObject.getName(ctx), "You can't delete the alone 'output' for " + domainObject.getInfo(ctx, "to[IMS_QP_ExpectedResult2QPTask].from.name"));
+                }
             }
 
-//            check table row ids
-            String message = "you can't delete expected result another owners.";
+            //            check table row ids
             for (int i = 0; i < expectedResultIDs.length; i++) {
+                LOG.info("relatedExpectedResultIDs: " + relatedExpectedResultIDs);
                 if (relatedExpectedResultIDs.containsKey(expectedResultIDs[i])) {
                     MqlUtil.mqlCommand(ctx, String.format("delete bus %s", expectedResultIDs[i]));
                 } else {
                     DomainObject expectedResultObject = new DomainObject(expectedResultIDs[i]);
-                    mapMessage.put(expectedResultObject.getName(ctx), message);
+                    LOG.info("mapMessage: " + !mapMessage.containsKey(expectedResultObject.getName(ctx)) + "|" + mapMessage);
+                    if (!mapMessage.containsKey(expectedResultObject.getName(ctx))) {
+                        mapMessage.put(expectedResultObject.getName(ctx), "you can't delete the expected result of another owners.");
+                        LOG.info("drop message: " + "you can't delete the expected result of another owners.");
+                    }
                 }
             }
 
@@ -95,7 +107,7 @@ public class IMS_QP_ExpectedResult_mxJPO {
             String type = object.getType(context);
             String state = "";
 
-//            IMS_QP_DEPTask
+            // IMS_QP_DEPTask
             if ("IMS_QP_DEPTask".equals(type)) {
                 result = IMS_QP_Security_mxJPO.currentUserIsDEPOwner(context, object) ||
                         IMS_QP_Security_mxJPO.currentUserIsQPSuperUser(context);
@@ -105,7 +117,7 @@ public class IMS_QP_ExpectedResult_mxJPO {
                         IMS_QP_Constants_mxJPO.RELATIONSHIP_IMS_QP_DEP2DEPProjectStage));
             }
 
-//            IMS_QP_QPTask
+            // IMS_QP_QPTask
             else if ("IMS_QP_QPTask".equals(type)) {
                 result = IMS_QP_Security_mxJPO.isOwnerQPlanFromTaskID(context, id) ||
                         IMS_QP_Security_mxJPO.currentUserIsQPSuperUser(context);
